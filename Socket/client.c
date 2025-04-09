3c848af5caf9ed892429bdc147c9ef81a2213710
Aayush Shukla <me@a2ys.dev>
2025-04-09 20:08:02 +0530

    fix: from UDP to TCP

--- Socket/client.c
+++ Socket/client.c
@@ -1,15 +1,20 @@
-#include <stdio.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<unistd.h>
+#include<arpa/inet.h>
 
-int main() {
-  int s = socket(AF_INET, SOCK_DGRAM, 0);
+int main(){
+  int s = socket(AF_INET, SOCK_STREAM, 0);
   struct sockaddr_in serv = {AF_INET, htons(7697), inet_addr("127.0.0.1")};
+  connect(s, (void*)&serv, sizeof(serv));
 
-  char b[50];
-  fgets(b, 50, stdin);  
-  sendto(s, b, 50, 0, (void*)&serv, sizeof(serv));
+  char buf[20];
+  printf("Enter: "); 
+  fgets(buf, 20, stdin);
+  write(s, buf, 20);
+
   close(s);
+  return 0;
 }
 
--- Socket/server.c
+++ Socket/server.c
@@ -1,18 +1,24 @@
-#include <stdio.h>
-#include <netinet/in.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<unistd.h>
 
-int main() {
-  int s = socket(AF_INET, SOCK_DGRAM, 0);
-  struct sockaddr_in a = {AF_INET, htons(7697), INADDR_ANY};
-  bind(s, (void*)&a, sizeof(a));
-  
-  char b[50];
-  struct sockaddr_in c;
-  socklen_t l = sizeof(c);
-  
-  recvfrom(s, b, 50, 0, (void*)&c, &l);
-  printf("%s", b);
+int main(){
+  int s = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in serv = {AF_INET, htons(7697), INADDR_ANY}, cli;
+
+  bind(s, (void*)&serv, sizeof(serv));
+  listen(s, 1);
+
+  int c, len = sizeof(cli);
+  c = accept(s, (void*)&cli, &len);
+
+  char buf[20] = "";
+  read(c, buf, 20);
+
+  printf("Reg No: 12345678\nMsg: %s", buf);
+
   close(s);
+  return 0;
 }
 
